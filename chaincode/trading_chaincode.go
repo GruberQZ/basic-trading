a00d966846db52bd22c6acf356075baf20879613
Blake <ambsneakysnack@gmail.com>
2016-11-23 21:01:02 -0500

    Invoke parent function, Query, read, Delete complete

--- chaincode/trading_chaincode.go
+++ chaincode/trading_chaincode.go
@@ -106,8 +106,148 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	fmt.Println("Invoke() is running: " + function)
 
 	// Handle all of the different function calls
-	// if function == "init" {
-	//
-	// }
+	// Initialize the state of the chaincode (RESET)
+	if function == "init" {
+		// Call the Init() function
+		return t.Init(stub, "init", args)
+	} else if function == "delete" { // Delete an entity from
+		// Call the Delete() function
+		res, err := t.Delete(stub, args)
+		// Ensure all open trades are still valid after deletion
+		cleanTrades(stub)
+		// Return result from Delete()
+		return res, err
+	} else if function == "write" { // Write a value to the chaincode state
+		// Pass arguments along to the Write() function
+		return t.Write(stub, args)
+	} else if function == "init_energy" { // Create a new energy block
+		// Pass arguments along to the init_energy function
+		return t.init_energy(stub, args)
+	} else if function == "set_owner" { // Transfer ownership of energy block
+		// Call the set_owner() function
+		res, err := t.set_owner(stub, args)
+		// Make sure open trades are still valid after ownership changes
+		cleanTrades(stub)
+		// Return result from set_owner()
+		return res, err
+	} else if function == "open_trade" { // Create a new trade order
+		// Pass arguments along to the open_trade function
+		return t.open_trade(stub, args)
+	} else if function == "perform_trade" { // Fulfill an open trade order
+		// Pass arguments along to the perform_trade function
+		res, err := t.perform_trade(stub, args)
+		// Make sure open trades are still valid after trade resolves
+		cleanTrades(stub)
+		// Return result from perform_trade()
+		return res, err
+	} else if function == "remove_trade" { // Cancel an open trade order
+		// Pass arguments along to the remove_trade function
+		return t.remove_trade(stub, args)
+	}
+
+	// Print error message if function not found
+	fmt.Println("Invoke() did not find function: " + function)
+
+	// Return error
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+// Query function
+// Handles all query type functions
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	// Debug message
+	fmt.Println("Query() is running: " + function)
+
+	// Handle the different types of query functions
+	if function == "read" {
+		return t.read(stub, args)
+	}
+
+	// Print message if query function not found
+	fmt.Println("Query() did not find function: " + function)
+
+	// Return on error
+	return nil, errors.New("Received unknown query function: " + function)
+}
+
+// Read function
+// Read a variable from chaincode state
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+
+	// Check to make sure number of arguments is correct
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1: name of variable to query")
+	}
+
+	// Get the variable from the chaincode state
+	name = args[0]
+	valAsBytes, err := stub.GetState(name)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	// Successful return
+	return valAsBytes, nil
+}
+
+// Delete function
+// Remove a key/value pair from the chaincode state
+func (t *SimpleChaincode) Delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	// Check number of arguments passed in
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1: key/value pair to delete")
+	}
+
+	// Remove key from the chaincode state
+	name := args[0]
+	err := stub.DelState(name)
+	if err != nil {
+		return nil, errors.New("Failed to delete state: " + name)
+	}
+
+	// Get the energy index
+	energyAsBytes, err := stub.GetState(energyIdStr)
+	if err != nil {
+		return nil, errors.New("Failed to get energy index")
+	}
+
+	// Turn the energy index into a string array
+	var energyIndex []string
+	json.Unmarshal(energyAsBytes, &energyIndex)
+
+	// Iterate through the energy index looking for the energy asset
+	for i, val := range energyIndex {
+		// Debug message
+		fmt.Println(strconv.Itoa(i) + " - looking at " + val + " for " + name)
+		// Determine if this is the correct energy asset
+		if val == name {
+			fmt.Println("Found energy asset to remove")
+			// Reconstruct the energyIndex to include everything except asset to be deleted
+			energyIndex = append(energyIndex[:i], energyIndex[i+1:]...)
+			// Debug: Print out all of the assets in the energyIndex
+			fmt.Println("New state of energy assets:")
+			for x := range energyIndex {
+				fmt.Println(string(x) + " - " + energyIndex[x])
+			}
+			// Found asset to remove, break
+			break
+		}
+	}
+
+	// Turn energyIndex back into JSON for chaincode state
+	jsonAsBytes, _ := json.Marshal(energyIndex)
+	err = stub.PutState(energyIdStr, jsonAsBytes)
+	// Successful exit
+	return nil, nil
+}
+
+// Write function
+// Write variables into the chaincode state
+func (t *SimpleChaincode) Write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, value string
+	var err error
+
 }
