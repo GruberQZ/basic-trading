0de9eac0159d4eec1fed6572aca80b51ab4dbcbc
Blake Loewengruber <ambsneakysnack@gmail.com>
2016-12-01 22:15:33 -0500

    Charging simulator V1 ready for testing

--- raspberrypi/charging-simulator.py
+++ raspberrypi/charging-simulator.py
@@ -1,5 +1,42 @@
 import RPi.GPIO as GPIO
 import time
+import requests
+import json
+import sys
+
+# Define destination URL
+url = 'https://b5b1f30cd80c4041972890286eb7e5df-vp0.us.blockchain.ibm.com:5001/chaincode'
+
+# Build the request object to POST
+reqObj = {}
+reqObj["jsonrpc"] = "2.0"
+reqObj["method"] = "query"
+reqObj["params"] = {}
+reqObj["params"]["type"] = 1
+reqObj["params"]["chaincodeID"] = {}
+reqObj["params"]["chaincodeID"]["name"] = "12decb80905604f0f7ea238c51e8cd4c7496640865ba48b8798d8c35eb9be86b206c7119c925aa115513941a5c10dc9af34e6530ced4e673db2376553aa156ce"
+reqObj["params"]["ctorMsg"] = {}
+reqObj["params"]["ctorMsg"]["function"] = "get_charging_trade"
+reqObj["params"]["ctorMsg"]["args"] = []
+reqObj["params"]["secureContext"] = "user_type1_1"
+reqObj["id"] = 0
+
+# Build the complete charging object
+completeObj = {}
+completeObj["jsonrpc"] = "2.0"
+completeObj["method"] = "invoke"
+completeObj["params"] = {}
+completeObj["params"]["type"] = 1
+completeObj["params"]["chaincodeID"] = {}
+completeObj["params"]["chaincodeID"]["name"] = "12decb80905604f0f7ea238c51e8cd4c7496640865ba48b8798d8c35eb9be86b206c7119c925aa115513941a5c10dc9af34e6530ced4e673db2376553aa156ce"
+completeObj["params"]["ctorMsg"] = {}
+completeObj["params"]["ctorMsg"]["function"] = "complete_charging_trade"
+completeObj["params"]["ctorMsg"]["args"] = []
+completeObj["params"]["secureContext"] = "user_type1_1"
+completeObj["id"] = 0
+
+# Encode object to json string
+json_data = json.dumps(reqObj)
 
 # Turn off warnings because this is the only script altering GPIO
 GPIO.setwarnings(False)
@@ -15,13 +52,63 @@ yellow4 = 20
 green = 21
 
 # Set up inputs
-chan_list = [red,yellow1,yellow2,yellow3,yellow4,green]
-GPIO.setup(chan_list, GPIO.OUT)
+all_leds = [red,yellow1,yellow2,yellow3,yellow4,green]
+done_charging_leds = [yellow1,yellow2,yellow3,yellow4,green]
+GPIO.setup(all_leds, GPIO.OUT)
 
-# Turn on all LEDs, wait a second, turn them all off again
-GPIO.output(chan_list, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(chan_list, GPIO.LOW)
+def chargeTheCar(seconds):
+    # Charging sequence
+    GPIO.output(red, GPIO.LOW)
+    GPIO.output(yellow1, GPIO.HIGH)
+    time.sleep(seconds/5)
+    GPIO.output(yellow2, GPIO.HIGH)
+    time.sleep(seconds/5)
+    GPIO.output(yellow3, GPIO.HIGH)
+    time.sleep(seconds/5)
+    GPIO.output(yellow4, GPIO.HIGH)
+    time.sleep(seconds/5)
+    GPIO.output(green, GPIO.HIGH)
+    time.sleep(seconds/5)
+    # Complete Sequence
+    for i in range(4):
+        # Turn off everything for .5 second
+        GPIO.output(all_leds, GPIO.LOW)
+        time.sleep(.25)
+        # Turn on everything for .5 seconds
+        GPIO.output(done_charging_leds, GPIO.HIGH)
+        time.sleep(.25)
+    # Turn off everything
+    GPIO.output(all_leds, GPIO.LOW)
+
+while True:
+    # Turn on red "standby" LED
+    GPIO.output(red, GPIO.HIGH)
+    time.sleep(8)
+    # Request data from Bluemix
+    r = requests.post(url, data=json_data)
+    # Turn response into an object
+    res = json.loads(r.text)
+    # Get result attribute
+    result = res.get('result')
+    # Get the message if it exists
+    try:
+        message = result.get('message')
+        # Turn the message into an object
+        messageObj = json.loads(message)
+        print(messageObj)
+        print("Amount to charge: " + str(messageObj.get('amount')))
+        print("Timestamp: " + str(messageObj.get('timestamp')))
+        print("ID: " + str(messageObj.get('id')))
+        # Complete charging
+        chargeTheCar(int(messageObj.get('amount'))/10)
+        # Update object params
+        completeObj["params"]["ctorMsg"]["args"] = [str(messageObj.get('timestamp')),str(messageObj.get('id'))]
+        # Send update to blockchain
+        postData = json.dumps(completeObj)
+        r = requests.post(url, data=postData)
+        print(r.text)
+    except:
+        print("Message not found in return object")
 
 # Clean up GPIO on exit
 GPIO.cleanup()
--- raspberrypi/request-testing.py
+++ raspberrypi/request-testing.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import sys
+import time
 
 # Define destination URL
 #url = 'https://api.github.com/events'
@@ -19,9 +21,60 @@ reqObj["params"]["ctorMsg"]["args"] = []
 reqObj["params"]["secureContext"] = "user_type1_1"
 reqObj["id"] = 0
 
+# Build the complete charging object
+completeObj = {}
+completeObj["jsonrpc"] = "2.0"
+completeObj["method"] = "invoke"
+completeObj["params"] = {}
+completeObj["params"]["type"] = 1
+completeObj["params"]["chaincodeID"] = {}
+completeObj["params"]["chaincodeID"]["name"] = "12decb80905604f0f7ea238c51e8cd4c7496640865ba48b8798d8c35eb9be86b206c7119c925aa115513941a5c10dc9af34e6530ced4e673db2376553aa156ce"
+completeObj["params"]["ctorMsg"] = {}
+completeObj["params"]["ctorMsg"]["function"] = "complete_charging_trade"
+completeObj["params"]["ctorMsg"]["args"] = []
+completeObj["params"]["secureContext"] = "user_type1_1"
+completeObj["id"] = 0
+
 # Encode object to json string
 json_data = json.dumps(reqObj)
 #print(json_data)
 
-r = requests.get(url)
-print(r.text)
+# Request data from Bluemix
+r = requests.post(url, data=json_data)
+# Turn response into an object
+res = json.loads(r.text)
+# Get result attribute
+result = res.get('result')
+# Get the message if it exists
+try:
+    message = result.get('message')
+    # Turn the message into an object
+    messageObj = json.loads(message)
+    print(messageObj)
+    print("Amount to charge: " + str(messageObj.get('amount')))
+    print("Timestamp: " + str(messageObj.get('timestamp')))
+    print("ID: " + str(messageObj.get('id')))
+    time.sleep(4)
+    # Complete charging
+    # Update object params
+    completeObj["params"]["ctorMsg"]["args"] = [str(messageObj.get('timestamp')),str(messageObj.get('id'))]
+    # Send update to blockchain
+    postData = json.dumps(completeObj)
+    r = requests.post(url, data=postData)
+    print(r.text)
+except:
+    print("Message not found in return object")
+    sys.exit()
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
